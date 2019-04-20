21f1fe9a4080a7eb67d87ff59236b3e2f865bee1
Shynet <shynet@gmail.com>
2019-04-20 15:33:38 +0300

    Worked on task kill

--- src/commands/showActiveNetworkTasks.command.ts
+++ src/commands/showActiveNetworkTasks.command.ts
@@ -4,6 +4,7 @@ import {
   ProcessManager,
   getProcessManagerForOS
 } from '../process-manager';
+import { MessageChannel } from 'worker_threads';
 
 export function registerCommand() {
   return vscode.commands.registerCommand(
@@ -26,10 +27,17 @@ export function registerCommand() {
             processManager
               .killProcess(message.pid as number)
               .then(result => {
-                if (result)
+                if (result) {
+                  // The process has been successfully killed, update the GUI
+                  panel.webview.postMessage({
+                    command: 'task_killed',
+                    pid: message.pid
+                  });
+
                   return vscode.window.showInformationMessage(
                     `Succesfully killed process with pid: ${message.pid}`
                   );
+                }
 
                 vscode.window.showErrorMessage(
                   `Failed to kill process with pid ${message.pid}!`
@@ -58,7 +66,7 @@ async function getWebViewContent(processes: Process[]) {
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Cat Coding</title>
+        <title>Active Network Tasks</title>
     </head>
     <body>
       <table class="table">
@@ -69,13 +77,13 @@ async function getWebViewContent(processes: Process[]) {
           <th scope="col">Local Address</th>
           <th scope="col">Foreign Address</th>
           <th scope="col">State</th>
-          <th scope="Kill task"></th>
+          <th scope="col">Functions</th>
         </tr>
       </thead>
       <tbody>
         ${processes.map(process => {
           return `
-          <tr>
+          <tr id="pid_${process.pid}">
             <td>${process.pid}</td>
             <td>${process.name}</td>
             <td>${process.localAddress}</td>
@@ -90,8 +98,20 @@ async function getWebViewContent(processes: Process[]) {
     </table>
 
       <script>
-      const vscode = acquireVsCodeApi();
-      
+        const vscode = acquireVsCodeApi();
+
+        window.addEventListener('message', event => {
+
+          const message = event.data;
+
+          switch (message.command) {
+              case 'task_killed':
+                  // Remove the entry as the task was killed
+                  document.querySelector('#pid_' + message.pid).remove();
+                  break;
+          }
+        });
+
         function onKillTask(pid) {
           vscode.postMessage({
             command: 'kill',
--- src/process-manager/windows.process-manager.ts
+++ src/process-manager/windows.process-manager.ts
@@ -4,43 +4,71 @@ import { Process } from './process';
 import { ProcessManager } from './process-manager';
 
 export class WindowsProcessManager extends ProcessManager {
-  constructor() {
-    super();
-  }
-
   getProcesses(): Promise<Process[]> {
-    const result = shell.exec('netstat -aon');
-
-    // Find the table start
-    const headerRegex = /Proto\s+Local Address\s+Foreign Address\s+State\s+PID/gi.exec(
-      result
-    ) as RegExpExecArray;
-
-    // Get the table string only by removing useless string
-    const headlessResult = result
-      .substring(headerRegex.index + headerRegex[0].length + 1)
-      .trim();
-    const entryRows = headlessResult.split(`\n`);
-
-    // Go through each line and map it's output into a process
-    const processes = entryRows.map(row => {
-      const rowData = row.match(/([^\s]+)/gi) as RegExpMatchArray;
-
-      return {
-        protocol: rowData[0],
-        localAddress: rowData[1],
-        foreignAddress: rowData[2],
-        state: rowData[3],
-        pid: parseInt(rowData[4])
-      } as Process;
-    });
+    const processes = this.getNetstatProcesses();
+    this.attachProcessNamesFromTasklist(processes);
 
     return Promise.resolve(processes);
   }
 
   killProcess(pid: number): Promise<boolean> {
-    const result = shell.exec(`taskkill -f -pid ${pid}`);
-    if (shell.error) return Promise.reject(shell.error);
-    return this.isProcessLive(pid);
+    shell.exec(`taskkill -f -pid ${pid}`);
+    const error = shell.error();
+    if (error) return Promise.reject(error);
+    return this.isProcessLive(pid).then(result => !result);
+  }
+
+  private getNetstatProcesses(): Process[] {
+    const result = shell.exec('netstat -aon');
+    const entryRows = result.split(`\n`).splice(4); // Remove the headers
+
+    // Go through each line and map it's output into a process
+    const processes = entryRows
+      .filter(row => !!row) // Filter out all empty rows
+      .map(row => {
+        const rowData = row.match(/([^\s]+)/gi) as RegExpMatchArray;
+
+        // If this is a process with no state, we should treat it differently
+        if (rowData.length < 5) {
+          return {
+            protocol: rowData[0],
+            localAddress: rowData[1],
+            foreignAddress: rowData[2],
+            pid: parseInt(rowData[3])
+          } as Process;
+        }
+
+        return {
+          protocol: rowData[0],
+          localAddress: rowData[1],
+          foreignAddress: rowData[2],
+          state: rowData[3],
+          pid: parseInt(rowData[4])
+        } as Process;
+      });
+
+    return processes;
+  }
+
+  private attachProcessNamesFromTasklist(processes: Process[]) {
+    // As this output does not contain any process name, we pull it using a different command
+    const result = shell.exec('tasklist').toString();
+
+    const entries = result.split('\n').splice(3);
+    const taskList = entries
+      .filter(row => !!row)
+      .map(row => {
+        const rowData = row.match(/([^\s]+)/gi) as RegExpMatchArray;
+        return {
+          name: rowData[0],
+          pid: parseInt(rowData[1])
+        };
+      }) as { name: string; pid: number }[];
+
+    // Fill all of the processes with their names
+    processes.forEach(proc => {
+      const foundTask = taskList.find(task => task.pid === proc.pid);
+      if (foundTask) proc.name = foundTask.name;
+    });
   }
 }
