3dc738669f1dc032b06ad2b0f09df323a8c237ff
Shynet <shynet@gmail.com>
2019-04-20 09:54:01 +0300

    Worked on windows process manager

--- src/process-manager/process.ts
+++ src/process-manager/process.ts
@@ -2,4 +2,8 @@ export interface Process {
     pid: number;
     name: string;
     port: number;
+    localAddress: string;
+    foreignAddress: string;
+    protocol: string;
+    state: string;
 }
--- src/process-manager/windows.process-manager.ts
+++ src/process-manager/windows.process-manager.ts
@@ -1,7 +1,5 @@
 import * as shell from 'shelljs';
 
-const tableParser = require('table-parser');
-
 import { Process } from './process';
 import { ProcessManager } from './process-manager';
 
@@ -9,13 +7,37 @@ export class WindowsProcessManager implements ProcessManager {
   getProcesses(): Promise<Process[]> {
     const result = shell.exec('netstat -aon');
 
-    const parsedTable = tableParser.parse(result);
+    // Find the table start
+    const headerRegex = /Proto\s+Local Address\s+Foreign Address\s+State\s+PID/gi.exec(
+      result
+    ) as RegExpExecArray;
 
-    return Promise.resolve([]);
+    // Get the table string only by removing useless string
+    const headlessResult = result
+      .substring(headerRegex.index + headerRegex[0].length + 1)
+      .trim();
+    const entryRows = headlessResult.split(`\n`);
+
+    const processes = entryRows.map(row => {
+      const rowData = row.match(/([^\s]+)/gi) as RegExpMatchArray;
+
+      return {
+        protocol: rowData[0],
+        localAddress: rowData[1],
+        foreignAddress: rowData[2],
+        state: rowData[3],
+        pid: parseInt(rowData[4])
+      } as Process;
+    });
+
+    return Promise.resolve(processes);
   }
+
   killProcess(process: Process): Promise<boolean> {
-    throw new Error('Method not implemented.');
+    const result = shell.exec(`taskkill -f -pid ${process.pid}`);
+    return Promise.resolve(true);
   }
+
   isProcessLive(process: Process): Promise<boolean> {
     throw new Error('Method not implemented.');
   }
